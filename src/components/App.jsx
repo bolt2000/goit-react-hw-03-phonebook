17604321bb169ce1e374bf0d7b67a07026890bf5
Fransua Boltof <bolt2000@ukr.net>
2023-05-15 09:26:09 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,22 @@ import Filter from './Filter/FilterContact';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
+
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+this.setState({ contacts: parsedContacts });
+    console.log(parsedContacts);
+  }
 
 
   componentDidUpdate(prevProps, prevState) {
